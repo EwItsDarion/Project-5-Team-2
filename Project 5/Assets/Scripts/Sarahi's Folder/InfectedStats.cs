57e5a1eedadb5919359ab8e3c70683cdae3a0fe2
Smurillo0 <144478992+Smurillo0@users.noreply.github.com>
2023-11-16 13:41:40 -0600

    wave system started

---
+++ Project 5/Assets/Scripts/Sarahi's Folder/InfectedStats.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Android.LowLevel;
+
+public class InfectedStats : MonoBehaviour
+{
+    private float scoreAddAmount = 10;
+
+    Spawner spawner;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
