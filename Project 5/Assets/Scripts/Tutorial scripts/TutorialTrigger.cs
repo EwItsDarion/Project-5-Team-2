c098f8ec1046c78ada8610be5f2cfb29caef42a2
Arthur-P-V <apetersonveatch@gmail.com>
2023-11-27 23:27:01 -0600

    Revert "Adds little battle with infected to tutorial"
    This reverts commit c64a0137b84895b0330ed09d9dacd20343d83231.

--- Project 5/Assets/Scripts/Tutorial scripts/TutorialTrigger.cs
+++ Project 5/Assets/Scripts/Tutorial scripts/TutorialTrigger.cs
@@ -6,7 +6,6 @@ public class TutorialTrigger : MonoBehaviour
 {
     public Weapon maskCannon;
     public GameObject entry3;
-    public GameObject spawner;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,7 +35,6 @@ public class TutorialTrigger : MonoBehaviour
             {
                 entry3.gameObject.SetActive(false);
                 maskCannon.gameObject.SetActive(true);
-                spawner.SetActive(true);
             }
             if (gameObject.name == "TutorialTrigger4")
             {
